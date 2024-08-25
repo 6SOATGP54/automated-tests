457cf1ad31557c606a1b667b8e791e136e8391ce
Cledersonbc <cledersonbc@live.com>
2024-08-25 19:21:46 -0300

    update: json files

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = defineConfig({
 	e2e: {
-		//baseUrl: 'http://localhost:8091/api',
-		baseUrl: 'https://2de6-186-220-40-123.ngrok-free.app/api',
+		baseUrl: 'http://localhost:8091/api',
+		//baseUrl: 'https://2de6-186-220-40-123.ngrok-free.app/api',
 		specPattern: 'cypress/e2e/**/*.feature',
 		log: true,
 		setupNodeEvents(on, config) {

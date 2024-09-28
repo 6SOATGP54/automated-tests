ae668d00130bd5b5e3169fe85357e76237e06d9b
Cledersonbc <cledersonbc@live.com>
2024-09-28 11:47:39 -0300

    added secrets and vars to github actions

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:8091/api',
-		//baseUrl: 'https://2de6-186-220-40-123.ngrok-free.app/api',
 		specPattern: 'cypress/e2e/**/*.feature',
 		log: true,
 		setupNodeEvents(on, config) {
